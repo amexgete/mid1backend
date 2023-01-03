ea30a036da9fda6614b7ea9b070c9627e3ad31ec
amexgete <amaregatie21@gmail.com>
2023-01-02 23:54:03 -0700

    mid1django

---
+++ student/serializers.py
@@ -0,0 +1,6 @@
+from .models import studentclass
+from rest_framework import serializers
+class stuSerializers(serializers.ModelSerializer):
+     class Meta:
+         model=studentclass
+         fields=['name','grade']
---
+++ student/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    
+    path('list/',views.studentList),
+    path('post/',views.AddStu),
+    path('update/',views.update),
+    path('detete/',views.deleteStu),
+      
+]
